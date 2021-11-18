683a710329e0e47223db0318ab44c109bb1b7139
FiryanulRizky <60762912+FiryanulRizky@users.noreply.github.com>
2021-11-19 00:59:41 +0800

    Ubud Souvenir Center v1.0

--- public_html/Super_Admin/visualisasi/pola_2item.php
+++ public_html/Super_Admin/visualisasi/pola_2item.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Visualisasi 2 Item</title>
+	<title>Aturan Asosiasi</title>
 	<link rel="stylesheet" href="../../css/style_visualisasi.css">
 	<link rel="stylesheet" href="../../css/bootstrap2.min.css">
 </head>
 <body>
+<?php include "../../db/koneksi.php";?>
+<div id="bgkonten">
 <?php
-include '../../db/koneksi.php';?>
+error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));?>
+
 <hr><div class="container"><form action="<?php $_SERVER["PHP_SELF"];?>" method="post"><div class="form-group"><div class="form-group"><label for="sel1">Lihat Pola berdasarkan Pengurutan/Sorting :</label> <?php
 				$teratas="";
 				$terbawah="";
@@ -40,18 +43,18 @@ include '../../db/koneksi.php';?>
 		if (isset($_POST['kolom'])) {
 			$result=trim($_POST['kolom']);
 			if ($_POST['kolom']=="persen_support") {
-				$list_pola=mysqli_query($conn,"SELECT * FROM infotoko,itemc2 WHERE infotoko.id_toko=itemc2.id_toko GROUP BY infotoko.id_toko ORDER BY COUNT(itemc2.id_itemc2) DESC");
+				$pola=mysqli_query($conn,"SELECT * FROM infotoko,itemc2 WHERE infotoko.id_toko=itemc2.id_toko GROUP BY infotoko.id_toko ORDER BY COUNT(itemc2.id_itemc2) DESC");
 				echo "Data Diurutkan berdasarkan <h3><i>Persentase Support/Penjualan Tertinggi</i></h3>";
 				?>
             	<form method="post" action=""><button type="submit" name="reset">Hapus Filter</button></form><br><?php		
 			}
 			else if ($_POST['kolom']=="support_count") {
-				$list_pola=mysqli_query($conn,"SELECT * FROM infotoko,itemc2 WHERE infotoko.id_toko=itemc2.id_toko GROUP BY infotoko.id_toko ORDER BY COUNT(itemc2.id_itemc2) ASC");
+				$pola=mysqli_query($conn,"SELECT * FROM infotoko,itemc2 WHERE infotoko.id_toko=itemc2.id_toko GROUP BY infotoko.id_toko ORDER BY COUNT(itemc2.id_itemc2) ASC");
 				echo "Data Diurutkan berdasarkan <h3><i>Persentase Support/Penjualan Terendah</i></h3>";
 				?>
             	<form method="post" action=""><button type="submit" name="reset">Hapus Filter</button></form><br><?php
 			} else if ($_POST['kolom']=="terbaik") {
-				$list_pola=mysqli_query($conn,"SELECT * FROM infotoko,itemc2,transaksi WHERE infotoko.id_toko=itemc2.id_toko AND infotoko.id_toko=transaksi.id_toko GROUP BY infotoko.id_toko ORDER BY COUNT(itemc2.id_itemc2 AND transaksi.id_transaksi) DESC");
+				$pola=mysqli_query($conn,"SELECT * FROM infotoko,itemc2,transaksi WHERE infotoko.id_toko=itemc2.id_toko AND infotoko.id_toko=transaksi.id_toko GROUP BY infotoko.id_toko ORDER BY COUNT(itemc2.id_itemc2 AND transaksi.id_transaksi) DESC");
 				echo "Data Diurutkan berdasarkan <h3><i>Seleksi Terbaik (Persentase Support Tertinggi & Transaksi Terbanyak)</i></h3>";
 				?>
             	<form method="post" action=""><button type="submit" name="reset">Hapus Filter</button></form><br><?php
@@ -59,75 +62,109 @@ include '../../db/koneksi.php';?>
 		}
 
 	else {
-$list_pola=mysqli_query($conn,"SELECT * FROM infotoko"); }
-while ($ambil_pola=mysqli_fetch_array($list_pola)) {
-$idtoko=$ambil_pola['id_toko'];
-$nama_toko=$ambil_pola['nama_toko'];
-echo'<table style="border:1px solid;">';
-	$cek=mysqli_query($conn,"SELECT * FROM itemc2 WHERE id_toko = '$idtoko' AND lift_ratio>=1");
-	if(mysqli_num_rows($cek)>0) {
-	    echo'<tr><td>';
-	echo'<center><a href="tabel_pola_2.php"><H2 style="color:black;">Pola Penjualan : '.$nama_toko.' (KDTK'.$idtoko.')</H2></a><br></center>';
-	$strRule=mysqli_query($conn,"SELECT * FROM rule WHERE id_toko='$idtoko' AND kdrule='R2'");
-		$dataRule=mysqli_fetch_array($strRule,MYSQLI_ASSOC);
-		$nRule=$dataRule['minsupport'];
 
-        $result=mysqli_query($conn,"SELECT * FROM itemc2 WHERE id_toko='$idtoko' AND persen_support >='$nRule' AND lift_ratio>=1 ORDER BY persen_support DESC LIMIT 5");
-    	
+$pola=mysqli_query($conn,"SELECT * FROM infotoko"); }
+
+while($ambil_pola=mysqli_fetch_array($pola)) {
+    $idtoko=$ambil_pola['id_toko'];
+    $nama_toko=$ambil_pola['nama_toko'];
+    $no="";
+    $no1="";
+$transaksi=mysqli_query($conn,"SELECT * FROM transaksi WHERE id_toko='$idtoko' GROUP BY kdtransaksi");
+  $strRule=mysqli_query($conn,"SELECT * FROM rule WHERE id_toko ='$idtoko' AND kdrule='R2'");
+        $dataRule=mysqli_fetch_array($strRule,MYSQLI_ASSOC); 
+        $nRule=$dataRule['minsupport'];
+        $result=mysqli_query($conn,"SELECT * FROM itemc2 WHERE id_toko ='$idtoko' AND persen_support >='$nRule' AND lift_ratio>=1 ORDER BY persen_support DESC LIMIT 5"); 
+        ?>
+<?php if (mysqli_num_rows($result)>0) {
+?>
+<div class="view_data">
+<table style="border-color:#399;width:100%;">
+    <tr><td colspan="6"><?php echo "<hr><center><a href='pola_2item.php'><H2 style='color:black;'>Pola Penjualan ".$nama_toko." (KDTK".$idtoko.")</H2></a></center><hr>"; ?></td></tr>
+<tr style="background-color: lightgrey;text-align: center;">
+  <td colspan="6">Table 2 : Aturan Asosiasi Final dari 2 Itemset</td>
+  </tr>
+<tr style="background-color: lightgrey;text-align: center;">
+            <td width="25">No.</td>
+			<td width="459">Aturan Final yang didapat</td>
+			<td colspan="2"><span class="style7">Confidence</span></td>
+            <td width="150">Lift Ratio</td>
+		</tr>
+	<?php
+    if (mysqli_num_rows($result)>0) {
 		while($rowC2 = mysqli_fetch_array($result,MYSQLI_ASSOC))
-		{
+		{ 
 		?>
-		<?php
+		<tr style="text-align: center;">
+			<td><?php echo ++$no1; ?></td>
+            <td><?php
             $C2kditem1=$rowC2['kditem1']; $C2kditem2=$rowC2['kditem2'];
 			//menampilkan data kditem1 c2
-			$MerkItem1=mysqli_query($conn,"SELECT * FROM item WHERE id_toko='$idtoko' AND kditem='$C2kditem1'");
-			?><div id="circle_2kiri"><div class="circle_2itemkiri">
-			<?php
-			$DataMerkItem1=mysqli_fetch_array($MerkItem1,MYSQLI_ASSOC); echo "[".$DataMerkItem1['kditem']."]"."<br><img src='../../gambar/produk/".$DataMerkItem1['gambar_item']."'><br>".$DataMerkItem1['merk'].",";?> </div><?php
+			$MerkItem1=mysqli_query($conn,"SELECT * FROM item WHERE id_toko ='$idtoko' AND kditem='$C2kditem1' ");
+			echo "Jika Membeli ";
+			$DataMerkItem1=mysqli_fetch_array($MerkItem1,MYSQLI_ASSOC); echo "[".$DataMerkItem1['kditem']."]".$DataMerkItem1['merk']." , ";
 			//menampilkan data kditem2 c2
+			echo " Maka Juga Membeli ";
+			$MerkItem2=mysqli_query($conn,"SELECT * FROM item WHERE id_toko ='$idtoko'AND kditem='$C2kditem2' ");
+			$DataMerkItem2=mysqli_fetch_array($MerkItem2,MYSQLI_ASSOC); echo "[".$DataMerkItem2['kditem']."]".$DataMerkItem2['merk'];
+			?></td>
+			<td width="118"><?php
+            //mencari nilai confidence
 			$kditem=$C2kditem1;
 			$query_T=mysqli_query($conn,"SELECT * FROM transaksi WHERE id_toko='$idtoko' AND kditem='$kditem' ");
 			$num_T=mysqli_num_rows($query_T);
-			$support_count=$rowC2['support_count'];
-			$Confidence=$support_count/$num_T*100; $persensupport = substr($Confidence,0,5)."%";
-			if ($persensupport <= 25) {
-				?> <div class="circle_hijau"><br><H2>H</H2><?php echo "<h3>membeli $persensupport</h3>"?></div>
-			<?php } ?>
-			<?php if ($persensupport > 25 && $persensupport <=50 ) { ?>
-				<div class="circle_kuning"><br><H2>K</H2><?php echo "<h3>membeli $persensupport</h3>"?></div>
-			<?php } ?>
-			<?php if ($persensupport > 50 && $persensupport <=75 ) { ?>
-				<div class="circle_oranye"><H2>O</H2><?php echo "<h3>membeli $persensupport</h3>"?></div>
-			<?php } ?>
-			<?php if ($persensupport > 75 ) { ?>
-				<div class="circle_merah"><H2>M</H2><?php echo "<h3>membeli $persensupport</h3>"?></div>
-			<?php } ?> <?php
-			$MerkItem2=mysqli_query($conn,"SELECT * FROM item WHERE kditem='$C2kditem2' ");
-			$DataMerkItem2=mysqli_fetch_array($MerkItem2,MYSQLI_ASSOC); ?></div> <?php
-			//sebaliknya
-			$MerkItem2=mysqli_query($conn,"SELECT * FROM item WHERE id_toko='$idtoko' AND kditem='$C2kditem2' ");
-			$DataMerkItem2=mysqli_fetch_array($MerkItem2,MYSQLI_ASSOC);?><div id="circle_2kiri_r"><div class="circle_2itemkiri_r"> <?php echo "[".$DataMerkItem2['kditem']."]"."<br><img src='../../gambar/produk/".$DataMerkItem2['gambar_item']."'><br>".$DataMerkItem2['merk'].",";?><br><br><br><hr></div><?php
-			
-			$MerkItem1=mysqli_query($conn,"SELECT * FROM item WHERE id_toko='$idtoko' AND kditem='$C2kditem1' ");
-			$kditem=$C2kditem2;
-			$query_T2=mysqli_query($conn,"SELECT * FROM transaksi WHERE kditem='$kditem' AND id_toko='$idtoko'");
-			$num_T2=mysqli_num_rows($query_T2);
-			$support_count2=$rowC2['support_count'];
-			$Confidence2=$support_count2/$num_T2*100; $persensupport2 = substr($Confidence2,0,5)."%";
-			if ($persensupport2 <= 25) {
-				?> <div class="circle_hijau"><br><H2>H</H2><?php echo "<h3>membeli $persensupport2</h3>"?></div>
-			<?php } ?>
-			<?php if ($persensupport2 > 25 && $persensupport2 <=50 ) { ?>
-				<div class="circle_kuning2"><br><H2>K</H2><?php echo "<h3>membeli $persensupport2</h3>"?></div>
-			<?php } ?>
-			<?php if ($persensupport2 > 50 && $persensupport2 <=75 ) { ?>
-				<div class="circle_oranye2"><H2>O</H2><?php echo "<h3>membeli $persensupport2</h3>"?></div>
-			<?php } ?>
-			<?php if ($persensupport2 > 75 ) { ?>
-				<div class="circle_merah2"><H2>M</H2><?php echo "<h3>membeli $persensupport2</h3>"?></div>
-			<?php } ?> <?php
+            $supp1=$rowC2['support_count'];
+             $cetak1 = $rowC2['support_count']."/".$num_T;
+            if($num_T!=0) {
+            echo $cetak1;
 
-			$DataMerkItem1=mysqli_fetch_array($MerkItem1,MYSQLI_ASSOC);?><div class="circle_2itemkanan_r"> <?php echo "[".$DataMerkItem1['kditem']."]"."<br><img src='../../gambar/produk/".$DataMerkItem1['gambar_item']."'><br>".$DataMerkItem1['merk'].","; echo"</div></div>"; } echo"</td></tr>"; 
-	} echo"</table>"; }?>
-	</body>
+			?></td>
+			<td width="105"><?php
+            $support_count=$rowC2['support_count'];
+            $Conf1=$support_count/$num_T;
+            $Confidence=$support_count/$num_T*100; echo substr($Confidence,0,5)."%";
+            echo"</td><td width='150'>";
+            echo $rowC2['lift_ratio'];
+         }
+			?></td>
+		</tr>
+		<tr style="text-align: center;">
+          <td><?php echo ++$no1;?></td>
+		  <td><?php
+          //mencari data kebalikannya
+			echo " Jika Membeli ";
+			$MerkItem2=mysqli_query($conn,"SELECT * FROM item WHERE id_toko ='$idtoko' AND kditem='$C2kditem2' ");
+			$DataMerkItem2=mysqli_fetch_array($MerkItem2,MYSQLI_ASSOC); echo "[".$DataMerkItem2['kditem']."]".$DataMerkItem2['merk'];
+			
+			$MerkItem1=mysqli_query($conn,"SELECT * FROM item WHERE id_toko ='$idtoko' AND kditem='$C2kditem1' ");
+			echo " Maka Juga Membeli ";
+			$DataMerkItem1=mysqli_fetch_array($MerkItem1,MYSQLI_ASSOC); echo "[".$DataMerkItem1['kditem']."]".$DataMerkItem1['merk']." , ";
+			//menampilkan data kditem2 c2
+		  ?></td>
+		  <td><?php
+            //mencari nilai confidence
+			$kditem=$C2kditem2;
+			$query_T2=mysqli_query($conn,"SELECT * FROM transaksi WHERE id_toko='$idtoko' AND kditem='$kditem' ");
+			$num_T2=mysqli_num_rows($query_T2); $cetak2 = $rowC2['support_count']."/".$num_T2;
+            if ($num_T2 !=0) {
+                echo $cetak2;
+			//while ($data_T=mysqli_fetch_array($query_T)){
+				//echo $data_T['kditem']."<br>";
+				//}
+			?></td>
+		  <td><?php
+            $support_count2=$rowC2['support_count'];
+            $Conf2=$support_count2/$num_T2;
+			$Confidence2=$support_count2/$num_T2*100; echo substr($Confidence2,0,5)."%"; 
+            echo"</td><td width='150'>";
+            echo $rowC2['lift_ratio'];
+            }
+			?></td>
+    </tr>
+		<?php } } } }
+    ?>
+</table>
+</div>
+</div>
+</body>
 </html>
