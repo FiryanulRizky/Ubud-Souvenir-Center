66dbd92fcee9a6129ca64c31f077f1b423212ce4
FiryanulRizky <60762912+FiryanulRizky@users.noreply.github.com>
2022-03-03 01:48:21 +0800

    Ubud Souvenir Center v1.0

--- public_html/header_session/logout.php
+++ public_html/header_session/logout.php
@@ -1,5 +1,5 @@
 <?php
-include"../login_inner.php";
+include"login_inner.php";
 	session_start();
 	$del_statistik=mysqli_query($conn,"DELETE FROM statistik WHERE id_toko='$idtoko'");
 	session_destroy();
