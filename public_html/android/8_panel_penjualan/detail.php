47f2b0ec3de3b4a487b3a6591d49380c3f6812d9
Firyanul Rizky <firyan2903@gmail.com>
2022-06-06 21:14:26 +0800

    Ubud Souvenir Center v1.0

--- public_html/android/8_panel_penjualan/detail.php
+++ public_html/android/8_panel_penjualan/detail.php
@@ -15,20 +15,16 @@ mysqli_query($conn,"UPDATE item SET views = views + 1  WHERE id_toko='".$_GET['i
 //echo $sql;
 $result = mysqli_query($conn,"SELECT * FROM item WHERE id_toko='".$_GET['idtoko']."' AND kditem='".$_GET['id']."'");
 
-
-
     // *** DISPLAY TABLE PRODUCTS
 echo '<div id=bgproduct_detail>';
 
     while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
         {
 
-
 echo'<div id="detail">';
- echo"<table><tr><td><a href=\"index.php?idtoko=".$_GET['idtoko']."\"><i class='fa fa-home'></i> Home</a>";unset($_SESSION['scari']);unset($_SESSION['scategory']); echo" &raquo; <a href=\"detail.php?idtoko=".$_GET['idtoko']."&id=".$row['kditem']."\" class=\"tbeli\">Detail Produk</a> &raquo; ".$row['jenis']." &raquo; ".$row['merk']."</table></td></tr>";
+ echo"<table><tr><td><a href=\"index.php?idtoko=".$_GET['idtoko']."&halaman_produk=1\"><i class='fa fa-home'></i> Home</a>";unset($_SESSION['scari']);unset($_SESSION['scategory']); echo" &raquo; <a href=\"detail.php?idtoko=".$_GET['idtoko']."&id=".$row['kditem']."\" class=\"tbeli\">Detail Produk</a> &raquo; ".$row['jenis']." &raquo; ".$row['merk']."</table></td></tr>";
    
-echo"</div>";  
-     
+echo"</div>";      
 echo"<table id='desc_singkat'>";
 	 echo"<tr><td><h1> ".$row['merk']."</h></td></tr>";
      echo"<tr><td>&raquo; Kode Barang: BR".$row['kditem']."</td></tr>";
@@ -40,9 +36,6 @@ echo'<table>';
      echo"</td>";	
      echo"</tr>";    
 echo"</table>";
-
- 
-
 echo"<div id='detail2'>";
 
 echo"<table id='tabledetail'>";
@@ -53,14 +46,10 @@ echo"<div id='hightlight'><i class='fa fa-tasks'></i>Detail</div>";
      echo"</tr>";
 echo"</table>";
 echo"<br>";
-echo "<a href=\"checkout_fisrt.php?action=add&idtoko=".$_GET['idtoko']."&id=".$row['kditem']."\" class=\"tambah\"><span><input type='button' value='Add To Cart $nama_toko' class='btn4'></span></a><br><br><a href=\"index.php?idtoko=".$_GET['idtoko']."&clear=y\" class=\"tambah\"><span><input type='button' value='Go To $nama_toko Menu' style='background-color:green;' class='btn4'></span></a></div>";
+echo "<a href=\"checkout_fisrt.php?action=add&idtoko=".$_GET['idtoko']."&id=".$row['kditem']."\" class=\"tambah\"><span><input type='button' value='Add To Cart $nama_toko' class='btn4'></span></a><br><br><a href=\"index.php?idtoko=".$_GET['idtoko']."&halaman_produk=1&clear=y\" class=\"tambah\"><span><input type='button' value='Go To $nama_toko Menu' style='background-color:green;' class='btn4'></span></a></div>";
 echo"</div>";
 }
 echo '</div>';
-
-
-
-
 echo '<div class="cleared"></div>';
 
 // *** LOAD PAGE FOOTER
