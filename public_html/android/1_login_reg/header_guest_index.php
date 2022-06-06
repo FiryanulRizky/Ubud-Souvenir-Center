d0743042958e272e884a9aa57936e8b855c5024d
Firyanul Rizky <firyan2903@gmail.com>
2022-06-06 20:14:58 +0800

    Ubud Souvenir Center v1.0

--- public_html/android/1_login_reg/header_guest_index.php
+++ public_html/android/1_login_reg/header_guest_index.php
@@ -74,15 +74,12 @@ date_default_timezone_set('Asia/Makassar');
 	
 	  <li><a href="index.php?clear=y"><img src="../../gambar/images/logo.png"></a></li>
 	  <li> <?php
-		  echo'<form method="post" action="index.php">
+		  echo'<form method="post" action="index.php?halaman_toko=1">
 			     <input class="btncari" type="submit" value=""><input class="texbox_cari" name="cari" value="'.$_SESSION['scari'].'" placeholder="  Type Here to Search" >
 			   </form>';
 	   ?></li><li style="margin-top:15px;font-size:24px;border:1px solid orange;padding:10px;"><a href="cara_belanja_index.php"><i class="fa fa-pencil-square-o"></i> How to Order</a></li>  
    </ul>  
 </div><!--End header conteent-->
-   
-
-
 	 
      </div><!--end header-->
 
