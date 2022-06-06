e9a2cf56463c71a9406a02b39ee4008d0eac11ea
Firyanul Rizky <firyan2903@gmail.com>
2022-06-06 20:15:41 +0800

    Ubud Souvenir Center v1.0

--- public_html/android/1_login_reg/header_guest_index.php
+++ public_html/android/1_login_reg/header_guest_index.php
@@ -72,7 +72,7 @@ date_default_timezone_set('Asia/Makassar');
 <div id="header_content">	<!--start header conteent-->
 	<ul>
 	
-	  <li><a href="index.php?clear=y"><img src="../../gambar/images/logo.png"></a></li>
+	  <li><a href="index.php?halaman_toko=1&clear=y"><img src="../../gambar/images/logo.png"></a></li>
 	  <li> <?php
 		  echo'<form method="post" action="index.php?halaman_toko=1">
 			     <input class="btncari" type="submit" value=""><input class="texbox_cari" name="cari" value="'.$_SESSION['scari'].'" placeholder="  Type Here to Search" >
